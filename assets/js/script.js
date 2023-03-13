597064ec871b8e46683054686ee61ed45b1e6289
Sam Jones <samejones2018@gmail.com>
2023-03-13 16:57:20 -0700

    Added a search bar and html clases to target for search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,22 @@
 $searchBtn = $('.search-btn');
-$weatherTemp = $('.weather-temp');
-
-$searchBtn.on('click', function() {
+$weatherTempBellingham = $('.weatherTempBellingham');
+$weatherCityBellingham = $('.weatherCityBellingham');
+$weatherIconBellingham = $('.weatherIconBellingham');
+// $searchBtn.on('click', function() {
     // Pull data from weather api and display it
     $.ajax({
 
-        url: 'http://api.openweathermap.org/data/2.5/weather?q=Washington,usa&APPID=25d33f8c57018604cd95eaaaa98fb241',
+        url: 'http://api.openweathermap.org/data/2.5/weather?q=bellingham,Washington,usa&APPID=25d33f8c57018604cd95eaaaa98fb241',
         type: 'GET',
         dataType: 'json',
         success: function(data) {
             console.log(data);
-            $weatherTemp.html(data.main.temp);
+            $weatherCityBellingham.html(data.name);
+            $weatherTempBellingham.html(data.main.temp);
+
         },
         error: function() {
             console.log('Error');
         }
     });
-});
+// });
