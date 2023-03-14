a3d279b9b5f62ef0e803d383dfd95be95b0d9479
Sam Jones <samejones2018@gmail.com>
2023-03-13 17:27:13 -0700

    added a feels like section and converted all units to faherinhite

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,19 @@ $searchBtn = $('.search-btn');
 $weatherTempBellingham = $('.weatherTempBellingham');
 $weatherCityBellingham = $('.weatherCityBellingham');
 $weatherIconBellingham = $('.weatherIconBellingham');
+$feelsLikeBellingham = $('.feelsLikeBellingham');
 // $searchBtn.on('click', function() {
     // Pull data from weather api and display it
     $.ajax({
 
-        url: 'http://api.openweathermap.org/data/2.5/weather?q=bellingham,Washington,usa&APPID=25d33f8c57018604cd95eaaaa98fb241',
+        url: 'http://api.openweathermap.org/data/2.5/weather?q=bellingham,Washington,usa&units=imperial&APPID=25d33f8c57018604cd95eaaaa98fb241',
         type: 'GET',
         dataType: 'json',
         success: function(data) {
             console.log(data);
-            $weatherCityBellingham.html(data.name);
-            $weatherTempBellingham.html(data.main.temp);
-
+            $weatherCityBellingham.html(data.name + ', ' + data.sys.country);
+            $weatherTempBellingham.html(data.main.temp + ' &deg;F');
+            $feelsLikeBellingham.html(data.main.feels_like + ' &deg;F');
         },
         error: function() {
             console.log('Error');
