9c6b0d26af84120bb6489dbbed3a1dceb8bc7e9f
Sam Jones <samejones2018@gmail.com>
2023-03-13 18:38:27 -0700

    added a functioning search bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,37 @@
-$searchBtn = $('.search-btn');
-$weatherTempBellingham = $('.weatherTempBellingham');
-$weatherCityBellingham = $('.weatherCityBellingham');
-$weatherIconBellingham = $('.weatherIconBellingham');
-$feelsLikeBellingham = $('.feelsLikeBellingham');
-// $searchBtn.on('click', function() {
-    // Pull data from weather api and display it
-    $.ajax({
+$(document).ready(function () {
 
-        url: 'http://api.openweathermap.org/data/2.5/weather?q=bellingham,Washington,usa&units=imperial&APPID=25d33f8c57018604cd95eaaaa98fb241',
-        type: 'GET',
-        dataType: 'json',
-        success: function(data) {
-            console.log(data);
-            $weatherCityBellingham.html(data.name + ', ' + data.sys.country);
-            $weatherTempBellingham.html(data.main.temp + ' &deg;F');
-            $feelsLikeBellingham.html(data.main.feels_like + ' &deg;F');
-        },
-        error: function() {
-            console.log('Error');
-        }
+    const $searchBtn = $('#search-button');
+    const $weatherTempBellingham = $('.weatherTempBellingham');
+    const $weatherCityBellingham = $('.weatherCityBellingham');
+    const $feelsLikeBellingham = $('.feelsLikeBellingham');
+
+
+    $searchBtn.on('click', function (event) {
+
+        event.preventDefault();
+
+        const cityZip = $("#search-input").val();
+
+
+        $.ajax({
+
+            url: `http://api.openweathermap.org/data/2.5/weather?zip=${cityZip}&units=imperial&APPID=25d33f8c57018604cd95eaaaa98fb241`,
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                //save data to local storage
+                localStorage.setItem('weatherData', JSON.stringify(data));
+                console.log(data);
+                //display data
+                $weatherCityBellingham.html(data.name + ', ' + data.sys.country);
+                $weatherTempBellingham.html(data.main.temp + ' &deg;F');
+                $feelsLikeBellingham.html(data.main.feels_like + ' &deg;F');
+            },
+            error: function () {
+                console.log('Error');
+            }
+
+        });
+
     });
-// });
+});
