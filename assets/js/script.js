52af4a6e6b384dee239c373e818fe0ac66ea1de0
Sam Jones <samejones2018@gmail.com>
2023-03-22 08:59:05 -0700

    temp cards now show, updated js to be able to update all the cards in less code, added dates, and a history

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,75 +6,89 @@ $(document).ready(function () {
     const $feelsLike = $('.feelsLike');
     const $dateCard = $('.date');
     const $todayDate = dayjs();
-    
-    const $temperatureCards = [".temperature1", ".temperature2", ".temperature3", ".temperature4", ".temperature5"];
-    const $descriptionCards = [".description:eq(0)", ".description:eq(1)", ".description:eq(2)", ".description:eq(3)", ".description:eq(4)"];
 
-    const $dateOne = $('.locationDateOne');
-    const $dateTwo = $('.locationDateTwo');
-    const $dateThree = $('.locationDateThree');
-    const $dateFour = $('.locationDateFour');
-    const $dateFive = $('.locationDateFive');
-
-    // Set today's date
+    // Set today's date on the element with class date using dayjs
     $dateCard.html(dayjs().format('dddd, MMMM D'));
 
-    const tomorrow = $todayDate.add(1, 'day');
-    const dayThree = $todayDate.add(2, 'day');
-    const dayFour = $todayDate.add(3, 'day');
-    const dayFive = $todayDate.add(4, 'day');
-    const daySix = $todayDate.add(5, 'day');
+    // Load search history
+    loadSearchHistory();
 
-    // Set the forecast dates
-    $dateOne.html(tomorrow.format('dddd, MMMM D'));
-    $dateTwo.html(dayThree.format('dddd, MMMM D'));
-    $dateThree.html(dayFour.format('dddd, MMMM D'));
-    $dateFour.html(dayFive.format('dddd, MMMM D'));
-    $dateFive.html(daySix.format('dddd, MMMM D'));
+    // Apply date to forecast cards
+    updateForecastDates();
 
-    function loadSearchHistory() {
-        const searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
-        $searchHistory.empty();
-        for (let city of searchHistory) {
-            $searchHistory.append(`<div class="search-history-item">${city}</div>`);
-        }
-    }
-
-    function saveSearchHistory(city) {
-        let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
-        if (!searchHistory.includes(city)) {
-            searchHistory.push(city);
-            localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
-            loadSearchHistory();
-        }
-    }
-
+    // Event listener for search button
     $searchBtn.on('click', function (event) {
         let $city = $('#city').val();
         event.preventDefault();
-        if ($city != '') {
-
-            $.ajax({
-
-                url: `https://api.openweathermap.org/data/2.5/forecast?q=${encodeURIComponent($city)}&units=imperial&appid=25d33f8c57018604cd95eaaaa98fb241`,
-                type: 'GET',
-                dataType: 'json',
-                success: function (data) {
-                    //save data to local storage
-                    localStorage.setItem('weatherData', JSON.stringify(data.city.name));
-                    //display data in console
-                    console.log(data);
-                    //display data
-                    $weatherCity.html(data.city.name + ', ' + data.city.country);
-                    $weatherTemp.html(data.list[0].main.temp + ' &deg;F');
-                    $feelsLike.html(data.list[0].main.feels_like + ' &deg;F');
-
-                },
-                error: function () {
-                    console.log('Error');
-                }
-
-            });
+        if ($city !== '') {
+            getWeatherData($city);
         }
     });
+
+    // Event listener for search history items
+    $searchHistory.on('click', '.history-item', function () {
+        getWeatherData($(this).text());
+    });
+
+    function updateForecastDates() {
+        const days = ['One', 'Two', 'Three', 'Four', 'Five'];
+        let currentDate = $todayDate;
+
+        days.forEach(function (day, index) {
+            currentDate = currentDate.add(1, 'day');
+            $(`.locationDate${day}`).html(currentDate.format('dddd, MMMM D'));
+        });
+    }
+
+    function getWeatherData(city) {
+        $.ajax({
+            url: `https://api.openweathermap.org/data/2.5/forecast?q=${encodeURIComponent(city)}&units=imperial&appid=25d33f8c57018604cd95eaaaa98fb241`,
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                // Save data to local storage
+                saveToSearchHistory(data.city.name);
+
+                // Display data
+                $weatherCity.html(data.city.name + ', ' + data.city.country);
+                $weatherTemp.html(data.list[0].main.temp + ' &deg;F');
+                $feelsLike.html(data.list[0].main.feels_like + ' &deg;F');
+
+                updateForecastDates();
+                displayForecast(data);
+            },
+            error: function () {
+                console.log('Error');
+            }
+        });
+    }
+
+    function saveToSearchHistory(city) {
+        let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+
+        if (searchHistory.indexOf(city) === -1) {
+            searchHistory.push(city);
+            localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+        }
+
+        loadSearchHistory();
+    }
+
+    function loadSearchHistory() {
+        let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+
+        $searchHistory.empty();
+        searchHistory.forEach(function (city) {
+            const $historyItem = $('<button>').addClass('history-item').text(city);
+            $searchHistory.append($historyItem);
+        });
+    }
+
+    function displayForecast(data) {
+        for (let i = 0; i < 5; i++) {
+            const forecastTemp = data.list[i * 8].main.temp;
+            const $temperature = $(`.temperature${i + 1}`);
+            $temperature.html(`${forecastTemp}&deg;F`);
+        }
+    }
 });
